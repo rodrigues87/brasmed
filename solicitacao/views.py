0c13db21ea2bdc87d70f642c95acdd2b205d966f
pedro <pedroh.mix@gmail.com>
2020-06-27 12:25:49 -0300

    initial commit

--- solicitacao/views.py
+++ solicitacao/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 from solicitacao.forms import SolicitacaoForm
+from django.contrib import messages
 
 
 def solicitacao(request):
@@ -11,7 +12,7 @@ def solicitacao(request):
             solicitacao = form.save(commit=False)
             solicitacao.vendedor = request.user
             solicitacao.save()
-
+            messages.success(request, 'Enviado Com Sucesso')
             return redirect('/')
         return render(request, 'solicitacao.html', {'form': form})
     return redirect('login/')
