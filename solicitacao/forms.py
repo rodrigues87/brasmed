30b33e82fd46e6a0313ce26061fe0fedf68d9afa
pedro <pedroh.mix@gmail.com>
2020-06-27 10:56:59 -0300

    initial commit

--- solicitacao/forms.py
+++ solicitacao/forms.py
@@ -6,5 +6,6 @@ class SolicitacaoForm(forms.ModelForm):
     class Meta:
         model = Solicitacao
         fields = ['nome_completo', 'identidade', 'cpf', 'data_de_nascimento', 'vendedor', 'modalidade_de_pagamento',
-                  'numero_do_cartao','nome_no_cartao','validade','codigo_de_seguranca','comprovante']
+                  'numero_do_cartao','nome_no_cartao','validade','codigo_de_seguranca','proposta_frente','proposta_verso',
+                  'foto_identidade','foto_cpf']
         # produtos = forms.ModelChoiceField(queryset=Produto.objects.all(), widget=forms.Select(attrs={'onchange': "myFunction();"}))
